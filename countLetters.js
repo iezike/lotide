1d25d24e45e34aba455c59f23122957674421c32
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-03-09 14:59:47 -0500

    Implementing countLetters in Lotide

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+// Assert function to validate output values
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`\u2757 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`\u2714 Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+/// Implementing Counting letters in a sensence 
+const countLetters = function(sentence) {
+  result = {};
+  for(let letter of sentence){
+    if(letter !== " ") {
+      if(result[letter]){
+        result[letter] +=1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+/// Testing code 
+const sentence = countLetters("lighthouse in the house")
+
+assertEqual(sentence['l'], 1);
+assertEqual(sentence['h'], 4);
+assertEqual(sentence['e'], 2); // expect this to fail.
+assertEqual(sentence['s'], 2);

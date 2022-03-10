c095fe3587fe27707e43bd4b42827f047fbcdc6d
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-03-09 19:37:01 -0500

    Creating program file to implement findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`\u2757 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`\u2714 Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let output = undefined;
+  for (let key in object) {
+    if (object[key] === value) {
+      output = key;
+    }
+  }
+  return output;
+};
+
+//Testing Code
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

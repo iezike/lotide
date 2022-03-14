8f6e57940e5f1f553d5cc254744ee63a4ed9f93d
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-03-14 08:05:02 -0400

    Refactored tail function file using module

--- tail.js
+++ tail.js
@@ -1,17 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`\u2757 Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`\u2714 Assertion Passed: ${actual} === ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual'); 
 
 const tail = function(input) {
   return input.slice(1);
 };
 
-//testing code
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail;
